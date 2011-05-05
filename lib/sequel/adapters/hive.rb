290972bc59bdd33a2cc28623e99ae41cf250e34e
hrp <hrparmar@gmail.com>
2011-05-05 13:45:13 -0700

    add column name and type support

--- lib/sequel/adapters/hive.rb
+++ lib/sequel/adapters/hive.rb
@@ -25,6 +25,13 @@ module Sequel
         end
       end
       alias_method :do, :execute
+      
+      def schema(table, opts={})
+        hero = execute("DESCRIBE #{table}")
+        hero[2..-1].map do |h|
+          [h.first.strip, {:db_type => h[1].strip.to_sym, :type => h[1].strip.to_sym}]
+        end
+      end
 
       def tables(opts={})
         execute('SHOW TABLES')
@@ -39,20 +46,34 @@ module Sequel
     
     class Dataset < Sequel::Dataset
       SELECT_CLAUSE_METHODS = clause_methods(:select, %w'distinct columns from join where group having compounds order limit')
+
+       CONVERT_FROM = { :string => :to_s, :bigint => :to_i }
+
+      def schema
+        @schema || @schema = @db.schema(@opts[:from].first)
+      end
+
+      def columns
+        return @columns if @columns
+        if @opts[:select].nil? || @opts[:select].include?(:*)
+          @columns = schema.map{|c| c.first}
+        else
+          @columns = @opts[:select]
+        end
+      end
+
+      def convert_type(column)
+        db_type = @schema.select{|a| a.first == column}.first.last[:db_type]
+        CONVERT_FROM[db_type]
+      end
       
       def fetch_rows(sql)
-        p @opts
         execute(sql) do |result|
           begin
             width = result.first.size
-            if @opts[:select] == :* || @opts[:select].nil?
-              @columns =  (0..width).to_a
-            else
-              @columns = @opts[:select]
-            end
             result.each do |r|
               row = {}
-              r.each_with_index {|v, i| row[@columns[i]] = v}
+              r.each_with_index {|v, i| row[columns[i]] = v.send(convert_type(columns[i]))}
               yield row
             end
           ensure
